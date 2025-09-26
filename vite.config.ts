54c7d96e2c196ea434cfb80e7f02307f4c7554b1
twistedwarden <twistedwarden626@gmail.com>
2025-09-26 18:40:08 +0800

    ewan

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://attendance-test-w0wn.onrender.com',
         changeOrigin: true,
         secure: false,
       },
