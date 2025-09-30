eeb8603b1dff4af36e66cbf88b5710ca8e3c01db
twistedwarden <twistedwarden626@gmail.com>
2025-10-01 01:30:01 +0800

    ewan

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // target: 'https://attendance-test-production.up.railway.app',
-        target: 'http://localhost:3000',
+        target: 'https://attendance-test-production.up.railway.app',
         changeOrigin: true,
         secure: false,
       },
